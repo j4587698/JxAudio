fbb0c0598b0970752cd62cc9340f33d6ad100a8d
jx <jx@jvxiang.com>
2024-03-12 22:12:11 +0800

    增加修改密码交易

---
+++ JxAudio.Web/Vo/ResetPasswordVo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JxAudio.Core.Entity;
+
+namespace JxAudio.Web.Vo;
+
+public class ResetPasswordVo
+{
+    public UserEntity? UserEntity { get; set; }
+    
+    [Required]
+    public string? NewPassword { get; set; }
+
+    [Required]
+    public string? RePassword { get; set; }
+}
