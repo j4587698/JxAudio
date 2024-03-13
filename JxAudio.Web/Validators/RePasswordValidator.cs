f05de3b4344d5944cb43c56ec7a2d3cc316c47a6
jx <jx@jvxiang.com>
2024-03-13 20:58:31 +0800

    更新修改密码

---
+++ JxAudio.Web/Validators/RePasswordValidator.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BootstrapBlazor.Components;
+using JxAudio.Core;
+using JxAudio.Web.Components.Pages;
+using JxAudio.Web.Vo;
+using Microsoft.Extensions.Localization;
+
+namespace JxAudio.Web.Validators;
+
+public class RePasswordValidator: IValidator
+{
+    public void Validate(object? propertyValue, ValidationContext context, List<ValidationResult> results)
+    {
+        if (context.ObjectInstance is ResetPasswordVo resetPasswordVo)
+        {
+            var localizer = Application.GetRequiredService<IStringLocalizer<UserManager>>();
+            switch (context.MemberName)
+            {
+                case nameof(ResetPasswordVo.RePassword):
+                    if (resetPasswordVo.RePassword != resetPasswordVo.NewPassword)
+                    {
+                        results.Add(new ValidationResult(localizer!["RePassword"], new []{context.MemberName}));
+                    }
+                    break;
+            }
+            
+        }
+    }
+}
