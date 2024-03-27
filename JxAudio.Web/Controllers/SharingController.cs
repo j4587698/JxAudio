d6bbd6a17c1d167065dcb5a5b7c452af18d1761a
jx <jx@jvxiang.com>
2024-03-27 16:54:56 +0800

    Sharing

---
+++ JxAudio.Web/Controllers/SharingController.cs
@@ -0,0 +1,31 @@
+﻿using JxAudio.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JxAudio.Web.Controllers;
+
+public class SharingController: AudioController
+{
+    [HttpGet("/getShares")]
+    public void GetShares()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+
+    [HttpGet("/createShare")]
+    public void CreateShare()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+
+    [HttpGet("/updateShare")]
+    public void UpdateShare()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+
+    [HttpGet("/deleteShare")]
+    public void DeleteShare()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+}
