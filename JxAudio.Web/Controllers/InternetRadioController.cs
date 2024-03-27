c812fe5c84d9e6949a8dfc08c0da27825180f5a5
jx <jx@jvxiang.com>
2024-03-27 17:03:33 +0800

    InternetRadio

---
+++ JxAudio.Web/Controllers/InternetRadioController.cs
@@ -0,0 +1,31 @@
+﻿using JxAudio.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JxAudio.Web.Controllers;
+
+public class InternetRadioController: AudioController
+{
+    [HttpGet("/getInternetRadioStations")]
+    public void GetInternetRadioStations()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/createInternetRadioStation")]
+    public void CreateInternetRadioStation()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/updateInternetRadioStation")]
+    public void UpdateInternetRadioStation()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/deleteInternetRadioStation")]
+    public void DeleteInternetRadioStation()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+}
