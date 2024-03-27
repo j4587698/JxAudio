ed2b784654527be4cd41f5a6a20b71f8b50c182f
jx <jx@jvxiang.com>
2024-03-27 16:57:44 +0800

    Podcast

---
+++ JxAudio.Web/Controllers/PodcastController.cs
@@ -0,0 +1,49 @@
+﻿using JxAudio.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JxAudio.Web.Controllers;
+
+public class PodcastController: AudioController
+{
+    [HttpGet("/getPodcasts")]
+    public void GetPodcasts()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/getNewestPodcasts")]
+    public void GetNewestPodcasts()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/refreshPodcasts")]
+    public void RefreshPodcasts()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/createPodcastChannel")]
+    public void CreatePodcastChannel()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/deletePodcastChannel")]
+    public void DeletePodcastChannel()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/deletePodcastEpisode")]
+    public void DeletePodcastEpisode()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/downloadPodcastEpisode")]
+    public void DownloadPodcastEpisode()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+}
