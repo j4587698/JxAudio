12ccef4e6642e99c10eb4c30e105529d6ebb0701
jx <jx@jvxiang.com>
2024-03-28 22:16:20 +0800

    制作TaskManager

---
+++ JxAudio.Web/Vo/ScheduleVo.cs
@@ -0,0 +1,20 @@
+﻿using Longbow.Tasks;
+
+namespace JxAudio.Web.Vo;
+
+public class ScheduleVo
+{
+    public string? Name { get; set; }
+
+    public SchedulerStatus Status { get; set; }
+
+    public DateTimeOffset? NextRuntime { get; set; }
+
+    public DateTimeOffset? LastRuntime { get; set; }
+
+    public TriggerResult LastRunResult{ get; set; }
+
+    public IScheduler? Scheduler { get; set; }
+
+    public DateTimeOffset CreatedTime { get; set; }
+}
