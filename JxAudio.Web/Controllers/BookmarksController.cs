d1439959a13057ec4212cd12b55c4721c79f2a98
jx <jx@jvxiang.com>
2024-03-28 10:18:52 +0800

    Bookmarks

---
+++ JxAudio.Web/Controllers/BookmarksController.cs
@@ -0,0 +1,37 @@
+﻿using JxAudio.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JxAudio.Web.Controllers;
+
+public class BookmarksController: AudioController
+{
+    [HttpGet("/getBookmarks")]
+    public void GetBookmarks()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/createBookmark")]
+    public void CreateBookmark()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/deleteBookmark")]
+    public void DeleteBookmark()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/getPlayQueue")]
+    public void GetPlayQueue()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+    
+    [HttpGet("/savePlayQueue")]
+    public void SavePlayQueue()
+    {
+        throw RestApiErrorException.NotImplemented();
+    }
+}
