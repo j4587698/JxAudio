e7befeb8fd973ba236c03b09bcb9f60995abfb24
jx <jx@jvxiang.com>
2024-03-29 17:24:41 +0800

    添加ISystemPlugin

---
+++ JxAudio.Plugin/ISystemPlugin.cs
@@ -0,0 +1,37 @@
+﻿namespace JxAudio.Plugin;
+
+/// <summary>
+/// 后台相关接口
+/// </summary>
+public interface ISystemPlugin
+{
+    /// <summary>
+    /// 插件被启用时
+    /// </summary>
+    void PluginEnable()
+    {
+    }
+    
+    /// <summary>
+    /// 插件被禁用时
+    /// </summary>
+    void PluginDisable()
+    {
+    }
+
+    /// <summary>
+    /// 插件被删除时
+    /// </summary>
+    void PluginDeleted()
+    {
+    }
+    
+    /// <summary>
+    /// 添加后台菜单项
+    /// </summary>
+    /// <returns></returns>
+    List<PluginMenuModel>? AddMenuItem()
+    {
+        return null;
+    }
+}
---
+++ JxAudio.Plugin/PluginMenuModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace JxAudio.Plugin;
+
+
+/// <summary>
+/// 插件添加菜单
+/// </summary>
+public class PluginMenuModel
+{
+    /// <summary>
+    /// 菜单Id
+    /// </summary>
+    public required string MenuId { get; set; }
+
+    /// <summary>
+    /// 菜单展示名
+    /// </summary>
+    public required string DisplayName { get; set; }
+
+    /// <summary>
+    /// 菜单图标
+    /// </summary>
+    public string Icon { get; set; } = "fas fa-fw fa-puzzle-piece";
+
+    /// <summary>
+    /// 插件的界面
+    /// </summary>
+    public required RenderFragment PluginBody { get; set; }
+    
+}
