ddf376a8f8c931d34200eab6de1527f408331d50
jx <jx@jvxiang.com>
2024-04-03 09:42:29 +0800

    增加AlistProvider

---
+++ Plugins/AListProviderPlugin/SystemInstance.cs
@@ -0,0 +1,20 @@
+﻿using BootstrapBlazor.Components;
+using JxAudio.Plugin;
+
+namespace AListProviderPlugin;
+
+[PluginInfo(Id = "D1D8F1CD-E92B-46DE-902F-128A00495DFD", Name = "Alist提供器", Author = "JxAudio", Description = "本地文件提供器", Version = "1.0")]
+public class SystemInstance: ISystemPlugin
+{
+    public List<PluginMenuModel>? AddMenuItem()
+    {
+        List<PluginMenuModel> list = new List<PluginMenuModel>();
+        list.Add(new PluginMenuModel()
+        {
+            DisplayName = "配置AList",
+            MenuId = "AListProviderPlugin",
+            PluginBody = BootstrapDynamicComponent.CreateComponent<AlistManager>().Render()
+        });
+        return list;
+    }
+}
