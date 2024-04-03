cd8d34151b36a5c3b4daf32d089eeca9c5bbcce4
jx <jx@jvxiang.com>
2024-04-03 17:29:26 +0800

    增加AList插件

---
+++ Plugins/AListProviderPlugin/AccountVo.cs
@@ -0,0 +1,11 @@
+﻿namespace AListProviderPlugin;
+
+public class AccountVo
+{
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    
+    
+}
---
+++ Plugins/AListProviderPlugin/Constants.cs
@@ -0,0 +1,30 @@
+﻿using JxAudio.Core.Entity;
+
+namespace AListProviderPlugin;
+
+public class Constants
+{
+    public const string Key = "AList";
+    
+    public static string? Token { get; set; }
+
+    public static async Task<AccountVo> GetAccount()
+    {
+        var values = await SettingsEntity.Where(x => x.GroupName == Key).ToListAsync();
+        var account = new AccountVo();
+        foreach (var value in values)
+        {
+            switch (value.SettingName)
+            {
+                case "UserName":
+                    account.UserName = value.SettingValue;
+                    break;
+                case "Password":
+                    account.Password = value.SettingValue;
+                    break;
+            }
+        }
+        
+        return account;
+    }
+}
