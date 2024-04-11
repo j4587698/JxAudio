dc35c769b466bad67fe3ccce811dfd1313574359
jx <jx@jvxiang.com>
2024-04-11 09:48:54 +0800

    修复默认封面没有的问题

--- JxAudio.Web/Controllers/Admin/CoverController.cs
+++ JxAudio.Web/Controllers/Admin/CoverController.cs
@@ -12,15 +12,20 @@ public class CoverController: ControllerBase
     {
         if (coverId is null or 0)
         {
-            return File(Path.Combine("Images", "NoCover.png"), "image/png");
+            return File(Constants.GetDefaultCover(), "image/png");
         }
 
         var picture = PictureEntity.Find(coverId.Value);
         if (picture == null)
         {
-            return File(Path.Combine("Images", "NoCover.png"), "image/png");
+            return File(Constants.GetDefaultCover(), "image/png");
         }
         var coverPath = Path.Combine(AppContext.BaseDirectory, Constants.CoverCachePath, picture.Path!);
+        if (!System.IO.File.Exists(coverPath))
+        {
+            return File(Constants.GetDefaultCover(), "image/png");
+        }
+        
         return File(System.IO.File.ReadAllBytes(coverPath), picture.MimeType!);
     }
 }
--- JxAudio.Web/Controllers/MediaLibraryScanningController.cs
+++ JxAudio.Web/Controllers/MediaLibraryScanningController.cs
@@ -2,6 +2,8 @@
 using JxAudio.Core.Entity;
 using JxAudio.Core.Subsonic;
 using JxAudio.Web.Extensions;
+using JxAudio.Web.Jobs;
+using Longbow.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JxAudio.Web.Controllers;
@@ -14,7 +16,7 @@ public class MediaLibraryScanningController: AudioController
         var count = await TrackEntity.Select.CountAsync(HttpContext.RequestAborted);
         var scanStatus = new ScanStatus()
         {
-            scanning = false,
+            scanning = TaskServicesManager.Get(nameof(ScanJob))?.LastRunResult == TriggerResult.Running,
             count = count,
             countSpecified = true,
         };
