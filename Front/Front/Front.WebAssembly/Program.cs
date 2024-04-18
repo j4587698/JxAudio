dfc127f1b42b4e4952c84b44e1c2857d9dfb96fd
jx <jx@jvxiang.com>
2024-04-18 17:32:59 +0800

    添加Front

---
+++ Front/Front/Front.WebAssembly/Program.cs
@@ -0,0 +1,17 @@
+﻿using Front.WebAssembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBootstrapBlazor(options =>
+{
+    options.IgnoreLocalizerMissing = true;
+    options.DefaultCultureInfo = "zh-CN";
+});
+
+await builder.Build().RunAsync();
